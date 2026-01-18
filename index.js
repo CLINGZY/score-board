f5310a4fec3e28ff41122e644d3b2d609467b81c
CLINGZY <clintonuduophori@gmail.com>
2026-01-18 19:41:54 +0100

    no changes

---
+++ index.js
@@ -0,0 +1,38 @@
+
+let homeCount = 0
+let guestCount = 0 
+
+let homeScore = document.getElementById('homeScore')
+let guestScore = document.getElementById('guestScore')
+
+
+function home1() {
+    homeCount += 1
+    homeScore.textContent = homeCount
+}
+
+function home2() {
+    homeCount += 2
+    homeScore.textContent = homeCount
+}
+
+function home3() {
+    homeCount += 3
+    homeScore.textContent = homeCount
+    
+}
+
+function guest1() {
+    guestCount += 1
+    guestScore.textContent = guestCount
+}
+
+function guest2() {
+    guestCount += 2
+    guestScore.textContent = guestCount
+}
+
+function guest3() {
+    guestCount += 3
+    guestScore.textContent = guestCount
+}
